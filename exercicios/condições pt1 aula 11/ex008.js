6c165de3561b774cbd53aebecefc9fcf60b818f0
caio bonetto prazeres <137189052+caiopraz@users.noreply.github.com>
2023-11-21 14:55:00 -0300

    aula 11 JS

---
+++ exercicios/condições pt1 aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log(`A velocidade é de ${vel} km/h.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o sinto de segurança.`)
